4cbb5a9af7f3cbf0c21b122e210142fbb6bd8195
VuVanCuong <cuong@oec.co.jp>
2021-05-27 11:56:29 +0900

    test

--- test.js
+++ test.js
@@ -312,9 +312,7 @@ if ("test".length == 4) {
 }
 if ("test".length == 4) {
   console.log("test")
-}
-
-if ("test".length == 4) {
+}if ("test".length == 4) {
   console.log("test")
 }
 if ("test".length == 4) {
