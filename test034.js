2a8c0675f0389094ad7b6063a91a59286afd9756
VuVanCuong <cuong@oec.co.jp>
2021-05-27 18:15:19 +0900

    test123234test

--- test034.js
+++ test034.js
@@ -13,18 +13,7 @@ if ("test".length == 4) {
 if ("test".length == 4) {
   console.log("test")
 }
-if ("test".length == 4) {
-  console.log("test")
-}
-if ("test".length == 4) {
-  console.log("test")
-}
-if ("test".length == 4) {
-  console.log("test")
-}
-if ("test".length == 4) {
-  console.log("test")
-}
+
 if ("test".length == 4) {
   console.log("test")
 }
